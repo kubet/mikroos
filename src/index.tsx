da5d9fd2e132df4acacbb6da87cc558319a9af8a
Vukasin <vukasinkubet@gmail.com>
2026-04-20 13:47:43 +0200

    feat: add vercel web analytics

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,9 @@
 /* @refresh reload */
 import { render } from "solid-js/web";
+import { inject } from "@vercel/analytics";
 import "./index.css";
 import App from "./ui/App";
+
+inject();
 
 render(() => <App />, document.getElementById("root")!);
